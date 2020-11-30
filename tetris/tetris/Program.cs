46403bf03c459137ab191cc329118b62746e3d05
Романов Николай <nikolay.s.romanov@gmail.com>
2020-11-30 16:14:17 +0300

    вывод двух символов бес функций

--- tetris/tetris/Program.cs
+++ tetris/tetris/Program.cs
@@ -6,7 +6,25 @@ namespace tetris
     {
         static void Main(string[] args)
         {
-            
+            Console.SetWindowSize(40, 30); // размер консоли
+            Console.SetBufferSize(40, 30);//убирает полоски прокрутки
+
+            int x1 = 2;
+            int y1 = 3;
+            char c1 = '*';
+
+            Console.SetCursorPosition(x1, y1);//устанавливает курсор в консоле по заданной координате
+            Console.Write(c1);// записывает символ в консоле
+
+            int x2 = 2;
+            int y2 = 10;
+            char c2 = '*';
+
+            Console.SetCursorPosition(x2, y2);//устанавливает курсор в консоле по заданной координате
+            Console.Write(c2);// записывает символ в консоле
+
+
+            Console.ReadLine(); // перед закрытием консоли дождется нажатия клавиши
         }
     }
 }
